f76eb006b70f62029a40b112ec17666347439615
Raja <rajadurai.krishnamoorthy@lastminute.com>
2017-08-03 15:22:52 +0530

    Added sort method

--- src/main/org/yathiraja/algo/graph/TopologicalSort.java
+++ src/main/org/yathiraja/algo/graph/TopologicalSort.java
@@ -1,9 +1,15 @@
 package org.yathiraja.algo.graph;
 
+import java.util.List;
+
 /**
  * Created by rkrishnamoorthy on 03/08/17.
  */
 public class TopologicalSort {
 
+    public void sort(List<Integer> list) {
+
+        
+    }
 
 }
